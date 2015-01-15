b1e61266c2b4ec23c80d608faa496b366c713850
unknown <claudio.david@LONWRK071.virtuefusion.corp>
2015-01-15 15:15:26 +0000

    initial commit

---
+++ src/main/java/primeFactors/PrimeFactors.java
@@ -0,0 +1,22 @@
+package primeFactors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by claudio.david on 15/01/2015.
+ */
+public class PrimeFactors {
+    public static List<Integer> generate(int n)
+    {
+        List<Integer> primes = new ArrayList<Integer>();
+        for(int candidate = 2; n > 1; candidate++)
+        {
+            for(; n % candidate == 0; n/=candidate)
+            {
+                primes.add(candidate);
+            }
+        }
+        return primes;
+    }
+}
